2bfc456aca66274a6058c7028b4a949cebd5b25e
NgBaoAnn <baoan1real1@gmail.com>
2026-05-01 20:48:20 +0700

    chore(promotion): remove unused imports

--- promotion/src/test/java/com/yas/promotion/utils/ConstantsTest.java
+++ promotion/src/test/java/com/yas/promotion/utils/ConstantsTest.java
@@ -3,7 +3,6 @@ package com.yas.promotion.utils;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ConstantsTest {
 
