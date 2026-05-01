adfe41aad5f55da251d3f4cc2eeabc7d945b8831
NgBaoAnn <baoan1real1@gmail.com>
2026-05-01 20:48:20 +0700

    chore(search): remove unused imports

--- search/src/test/java/com/yas/search/controller/ProductControllerTest.java
+++ search/src/test/java/com/yas/search/controller/ProductControllerTest.java
@@ -4,7 +4,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
