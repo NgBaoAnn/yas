61dcb68165f56fe8a2f099cdddd11cf3976c6273
NgBaoAnn <baoan1real1@gmail.com>
2026-05-01 20:32:01 +0700

    refactor: remove redundant comment in StockServiceTest

--- inventory/src/test/java/com/yas/inventory/service/StockServiceTest.java
+++ inventory/src/test/java/com/yas/inventory/service/StockServiceTest.java
@@ -6,7 +6,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
+//
 import com.yas.commonlibrary.exception.BadRequestException;
 import com.yas.commonlibrary.exception.NotFoundException;
 import com.yas.commonlibrary.exception.StockExistingException;
